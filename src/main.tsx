7473e433155874b42ab119de2cebee22e844a9fc
adicooks <109033004+adicooks@users.noreply.github.com>
2025-11-09 21:12:51 -0500

    vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,11 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./styles/typography.css";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById("root")!).render(<App />);
-
-
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
